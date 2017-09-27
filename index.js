edbcd678855327b4500d3c12faa412428a3356c6
Hoon Lee <yl.hoony@gmail.com>
2017-09-27 17:50:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Geek One';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
+
+shortGreeting;
