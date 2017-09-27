c549f55da74b7fa9a9a205258e9187bf82cdaa2b
Hoon Lee <yl.hoony@gmail.com>
2017-09-27 17:49:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
 
 shortGreeting;
